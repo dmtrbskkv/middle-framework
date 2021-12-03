83cbd987793060f34821805507a1b5382c0c5f33
Dmitry Baskakov <baskakovdima99@mail.ru>
2021-12-03 17:28:22 +1000

    update terminal shortcuts

--- config/terminal.php
+++ config/terminal.php
@@ -2,9 +2,10 @@
 
 return [
     'commands' => [
-        'db' => 'php vendor/bin/doctrine',
+//        'db' => 'php vendor/bin/doctrine',
+        'db' => './vendor/bin/doctrine-migrations ',
         'db:migrate' => './vendor/bin/doctrine-migrations migrate',
         'migration:create' => './vendor/bin/doctrine-migrations generate',
-        'models:validate' => 'php vendor/bin/doctrine orm:validate-schema ',
+//        'models:validate' => 'php vendor/bin/doctrine orm:validate-schema ',
     ],
 ];
